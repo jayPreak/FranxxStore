582be1d7925dea5ca6fdda8bd9e4fb74d4398c33
jayPreak <jayesh.preak2003@gmail.com>
2023-10-30 16:00:03 +0530

    stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 
 export default function Home() {
   return (
-    <div>
-      hii
-    </div>
+    <>
+      Banner
+
+      <div className="products-heading">
+        <h2>Best Selling</h2>
+        <p>FRANXXフランクス</p>
+      </div>
+
+      <div>
+        {['Prod 1', 'Prod 2', 'Prod 3'].map(
+          (prod, key) => <p key={key}>{prod}</p>
+        )}
+      </div>
+
+      Footer
+    </>
   )
 }

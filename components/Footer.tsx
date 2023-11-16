2dc83344dba17a4433f0c1595501b7352b8d976c
jayPreak <jayesh.preak2003@gmail.com>
2023-11-16 15:01:02 +0530

    xdcfvhgj

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { AiOutlineGithub, AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
+import { FaHeart } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="footer-container">
+            <p>
+                FranxxStore 2023 Made with &#10084;
+            </p>
+
+            <p className="icons">
+                <AiOutlineGithub />
+                <AiOutlineInstagram />
+                <AiOutlineTwitter />
+            </p>
+        </div>
     )
 }
 
